d136d83dabfcae5dbb97eff2299485b4ffd9f492
jonas <jonasgabriel9803@gmail.com>
2024-11-13 19:34:48 -0300

    commit start: curse

---
+++ script.js
@@ -0,0 +1,11 @@
+// Stp1
+console.log("Construindo uma piramide em Js basics");
+console.log("     #     ");
+console.log("    ###    ");
+console.log("   #####   ");
+console.log("  #######  ");
+console.log("###########");
+// Stp2
+// let character;
+// Stp3
+let character = "Hello";
